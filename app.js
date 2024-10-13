2190e3b06c047f2457d64b511f3192963de3476a
Abhay Bansal <targeter001@gmail.com>
2024-10-13 16:58:27 +0530

    adding a express rate limit

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ import bodyParser from "body-parser";
 import cors from "cors"; // Import cors package
 
 // Basic rate-limiting middleware for Express. Use to limit repeated requests to public APIs and/or endpoints such as password reset
-import { rateLimit } from "express-rate-limit";
 import Routes from "./api/routes";
+import { rateLimit } from "express-rate-limit";
 import startServer from "./startServer";
 
 const app = new Express();
@@ -14,16 +14,6 @@ const app = new Express();
 //added cors to avoid cors error
 app.use(cors());
 
-// Support parsing of application/json type post data
-app.use(bodyParser.json());
-
-// Support parsing of application/x-www-form-urlencoded post data
-app.use(bodyParser.urlencoded({ extended: true }));
-
-// Initialize Routes
-Routes.init(app);
-
-// Rate limiting middleware
 const limiter = rateLimit({
   windowMs: 1000, // 1 second window
   max: 5, // limit each IP to 5 requests per windowMs (adjust this as needed)
@@ -32,6 +22,16 @@ const limiter = rateLimit({
 });
 // apply limit to all request
 app.use(limiter);
+
+// Support parsing of application/json type post data
+app.use(bodyParser.json());
+// Rate limiting middleware
+
+// Support parsing of application/x-www-form-urlencoded post data
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Initialize Routes
+Routes.init(app);
 
 // Start Server
 startServer(app);
