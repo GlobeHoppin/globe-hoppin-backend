3968052e9035892c485de50eceaa198b71948732
Abhay Bansal <targeter001@gmail.com>
2024-10-13 16:43:47 +0530

    Implement basic rate limiting using express-rate-limit

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ import Express from "express";
 import bodyParser from "body-parser";
 import cors from "cors"; // Import cors package
 
+// Basic rate-limiting middleware for Express. Use to limit repeated requests to public APIs and/or endpoints such as password reset
+import { rateLimit } from "express-rate-limit";
 import Routes from "./api/routes";
 import startServer from "./startServer";
 
@@ -20,6 +22,16 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // Initialize Routes
 Routes.init(app);
+
+// Rate limiting middleware
+const limiter = rateLimit({
+  windowMs: 1000, // 1 second window
+  max: 5, // limit each IP to 5 requests per windowMs (adjust this as needed)
+  message: "Too many requests from this IP, please try again after a second",
+  headers: true, // Sends rate limit info in response headers
+});
+// apply limit to all request
+app.use(limiter);
 
 // Start Server
 startServer(app);
