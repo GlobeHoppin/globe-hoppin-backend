985ea99bef71313407b083a05cadc37afceb010a
Abhay Bansal <targeter001@gmail.com>
2024-10-13 17:47:46 +0530

    Final commit Updated rate Limiting to allow a maximum 5 requests per second

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ import cors from "cors"; // Import cors package
 
 // Basic rate-limiting middleware for Express. Use to limit repeated requests to public APIs and/or endpoints such as password reset
 import Routes from "./api/routes";
-import { rateLimit } from "express-rate-limit";
+import rateLimit from "express-rate-limit";
 import startServer from "./startServer";
 
 const app = new Express();
@@ -19,9 +19,23 @@ const limiter = rateLimit({
   max: 5, // limit each IP to 5 requests per windowMs (adjust this as needed)
   message: "Too many requests from this IP, please try again after a second",
   headers: true, // Sends rate limit info in response headers
+  handler: (req, res) => {
+    // Custom error response
+    return res.status(429).json({
+      error: "Rate limit exceeded",
+      message:
+        "You have exceeded the number of allowed requests. Please try again later.",
+    });
+  },
 });
 // apply limit to all request
 app.use(limiter);
+
+// log the changes
+// app.use((req, res, next) => {
+//   console.log(`[${new Date().toISOString()}] ${req.method} ${req.originalUrl}`);
+//   next();
+// });
 
 // Support parsing of application/json type post data
 app.use(bodyParser.json());
